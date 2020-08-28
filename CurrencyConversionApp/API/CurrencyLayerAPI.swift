c54590970ccf95f8a403bd5e62750bc04db30bb1
Enric Macias Lopez <enric_maciaslopez@cyberagent.co.jp>
2020-08-28 14:10:54 +0900

    Implements Currency Layer API requests

---
+++ CurrencyConversionApp/API/CurrencyLayerAPI.swift
@@ -0,0 +1,63 @@
+import RxAlamofire
+import RxSwift
+
+public final class CurrencyLayerAPI {
+    private let disposeBag = DisposeBag()
+
+    public static func requestCurrenciesList() -> Observable<[String: AnyObject]> {
+        let parameters = ["access_key": Const.accessKey]
+        return RxAlamofire.requestJSON(.get,
+                                       Const.currenciesListRequest,
+                                       parameters: parameters)
+            .flatMap { _, json -> Observable<[String: AnyObject]> in
+                guard let dict = json as? [String: AnyObject],
+                    let currencies = dict["currencies"] as? [String: AnyObject] else {
+                        // TODO: Create an error class, and handle errors more proeprly
+                        return .error(NSError(domain:"", code:0, userInfo:nil))
+                }
+                return .just(currencies)
+            }
+    }
+
+    public static func requestRates() -> Observable<[String: AnyObject]> {
+        let parameters = ["access_key": Const.accessKey]
+        return RxAlamofire.requestJSON(.get,
+                                       Const.ratesRequest,
+                                       parameters: parameters)
+            .debug()
+            .flatMap { _, json -> Observable<[String: AnyObject]> in
+                guard let dict = json as? [String: AnyObject],
+                    let currencies = dict["quotes"] as? [String: AnyObject] else {
+                        // TODO: Create an error class, and handle errors more proeprly
+                        return .error(NSError(domain:"", code:0, userInfo:nil))
+                }
+                return .just(currencies)
+            }
+    }
+    
+    public static func requestConversion() -> Observable<[String: AnyObject]> {
+        let parameters = ["access_key": Const.accessKey]
+        return RxAlamofire.requestJSON(.get,
+                                       Const.ratesRequest,
+                                       parameters: parameters)
+            .debug()
+            .flatMap { _, json -> Observable<[String: AnyObject]> in
+                guard let dict = json as? [String: AnyObject],
+                    let currencies = dict["quotes"] as? [String: AnyObject] else {
+                        // TODO: Create an error class, and handle errors more proeprly
+                        return .error(NSError(domain:"", code:0, userInfo:nil))
+                }
+                return .just(currencies)
+            }
+    }
+}
+
+// MARK: - Const
+
+extension CurrencyLayerAPI {
+    fileprivate enum Const {
+        static let accessKey = "08fa349892f3c1e40344e743de9d9c48"
+        static let currenciesListRequest = "http://api.currencylayer.com/list"
+        static let ratesRequest = "http://api.currencylayer.com/live"
+    }
+}
